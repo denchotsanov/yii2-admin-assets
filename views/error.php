a43066b5b2eff9ae500183f0df697bbded4538d6
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-06-19 09:01:41 +0300

    error page

--- views/error.php
+++ views/error.php
@@ -5,6 +5,16 @@
  * @var $name string
  * @var $message string
  * @var $exception \yii\web\HttpException
+ *
+ *  public function actions()
+ *  {
+ *       return [
+ *          "error" => [
+ *              "class" => \yii\web\ErrorAction::className(),
+ *              "view" => "@denchotsanov/views/error",
+ *          ],
+ *      ];
+ *  }
  */
 
 use yii\helpers\Html;
@@ -19,9 +29,9 @@ $textColor = $exception->statusCode === 404 ? "text-yellow" : "text-red";
         <h3><i class="fa fa-warning <?= $textColor ?>"></i> <?= nl2br(Html::encode($message)) ?></h3>
         <p>
             The above error occurred while the Web server was processing your request.
-        </p>
-        <p>
             Please contact us if you think this is a server error. Thank you.
+            Meanwhile, you may <a href='<?= Yii::$app->homeUrl ?>'>return to dashboard</a> or try using the search
+            form.
         </p>
     </div>
 </div>
---
+++ views/layouts/main.php
@@ -0,0 +1,71 @@
+<?php
+/**
+* @var $content string
+*/
+use yii\helpers\Html;
+
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
+
+?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php $this->head() ?>
+</head>
+<!--
+BODY TAG OPTIONS:
+=================
+Apply one or more of the following classes to get the
+desired effect
+|---------------------------------------------------------|
+| SKINS         | skin-blue                               |
+|               | skin-black                              |
+|               | skin-purple                             |
+|               | skin-yellow                             |
+|               | skin-red                                |
+|               | skin-green                              |
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | fixed                                   |
+|               | layout-boxed                            |
+|               | layout-top-nav                          |
+|               | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+-->
+<body class="hold-transition skin-blue sidebar-mini">
+<?php $this->beginBody() ?>
+<div class="wrapper">
+
+    <?= $this->render(
+        'header.php',
+        ['directoryAsset' => $directoryAsset]
+    ) ?>
+
+    <?= $this->render(
+        'left.php',
+        ['directoryAsset' => $directoryAsset]
+    )
+    ?>
+
+    <?= $this->render(
+        'content.php',
+        ['content' => $content, 'directoryAsset' => $directoryAsset]
+    ) ?>
+
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
