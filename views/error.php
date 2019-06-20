87d13db661058389352568611c63400109d49edd
Dencho Tsanov <d.tsanov@atomsolutions.net>
2019-06-20 12:48:31 +0300

    adminlte 3 update last branch

--- views/error.php
+++ views/error.php
@@ -19,18 +19,40 @@
 use yii\helpers\Html;
 
 $this->title = $name;
-$textColor = $exception->statusCode === 404 ? "text-yellow" : "text-red";
+$textColor = $exception->statusCode === 404 ? "text-yellow" : "text-danger";
 ?>
-
+<section class="content">
 <div class="error-page">
     <h2 class="headline <?= $textColor ?>"> <?= $exception->statusCode ?></h2>
     <div class="error-content">
-        <h3><i class="fa fa-warning <?= $textColor ?>"></i> <?= nl2br(Html::encode($message)) ?></h3>
+        <h3><i class="fas fa-exclamation-triangle <?= $textColor ?>"></i> <?= nl2br(Html::encode($message)) ?></h3>
         <p>
             The above error occurred while the Web server was processing your request.
             Please contact us if you think this is a server error. Thank you.
             Meanwhile, you may <a href='<?= Yii::$app->homeUrl ?>'>return to dashboard</a> or try using the search
             form.
         </p>
+        <form class="search-form">
+            <div class="input-group">
+                <input type="text" name="search" class="form-control" placeholder="Search">
+
+                <div class="input-group-append">
+                    <button type="submit" name="submit" class="btn btn-danger"><i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.input-group -->
+        </form>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+</section>
