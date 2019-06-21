9f8de69b7c9e86674bbbe47c315a4a67e72af39e
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-06-21 23:43:58 +0300

    update alert widget

--- widgets/Alert.php
+++ widgets/Alert.php
@@ -3,74 +3,74 @@
 
 namespace denchotsanov\widgets;
 
-use yii\bootstrap\Alert as BootstrapAlert;
-use yii\bootstrap\Widget;
-
+use Yii;
 /**
- * Alert widget renders a message from session flash for AdminLTE alerts. All flash messages are displayed
- * in the sequence they were assigned using setFlash. You can set message as following:
- *
  * ```php
- * \Yii::$app->getSession()->setFlash('error', '<b>Alert!</b> Danger alert preview. This alert is dismissable.');
+ * Yii::$app->session->setFlash('error', 'This is the message');
+ * Yii::$app->session->setFlash('success', 'This is the message');
+ * Yii::$app->session->setFlash('info', 'This is the message');
  * ```
  *
  * Multiple messages could be set as follows:
  *
  * ```php
- * \Yii::$app->getSession()->setFlash('error', ['Error 1', 'Error 2']);
+ * Yii::$app->session->setFlash('error', ['Error 1', 'Error 2']);
  * ```
- *
  */
-class Alert extends Widget
+class Alert extends \yii\bootstrap\Widget
 {
-    public $alertTypes = [
-        'error' => [
-            'class' => 'alert-danger',
-            'icon' => '<i class="icon fa fa-ban"></i>',
-        ],
-        'danger' => [
-            'class' => 'alert-danger',
-            'icon' => '<i class="icon fa fa-ban"></i>',
-        ],
-        'success' => [
-            'class' => 'alert-success',
-            'icon' => '<i class="icon fa fa-check"></i>',
-        ],
-        'info' => [
-            'class' => 'alert-info',
-            'icon' => '<i class="icon fa fa-info"></i>',
-        ],
-        'warning' => [
-            'class' => 'alert-warning',
-            'icon' => '<i class="icon fa fa-warning"></i>',
-        ],
+    public $alertIcons = [
+        'error'   => 'times-circle',
+        'danger'  => 'times-circle',
+        'success' => 'check-circle-o',
+        'info'    => 'info',
+        'warning' => 'exclamation-triangle'
     ];
-
+    public $alertMeanings = [
+        'error'   => '',
+        'danger'  => '',
+        'success' => '',
+        'info'    => '',
+        'warning' => ''
+    ];
+    /**
+     * @var array the alert types configuration for the flash messages.
+     * This array is setup as $key => $value, where:
+     * - $key is the name of the session flash variable
+     * - $value is the bootstrap alert type (i.e. danger, success, info, warning)
+     */
+    public $alertTypes = [
+        'error'   => 'alert-danger',
+        'danger'  => 'alert-danger',
+        'success' => 'alert-success',
+        'info'    => 'alert-info',
+        'warning' => 'alert-warning'
+    ];
+    /**
+     * @var array the options for rendering the close button tag.
+     */
     public $closeButton = [];
-
-    public $isAjaxRemoveFlash = true;
-
     public function init()
     {
         parent::init();
-        $session = \Yii::$app->getSession();
+        $session = Yii::$app->session;
         $flashes = $session->getAllFlashes();
         $appendCss = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
         foreach ($flashes as $type => $data) {
             if (isset($this->alertTypes[$type])) {
                 $data = (array) $data;
-                foreach ($data as $message) {
-                    $this->options['class'] = $this->alertTypes[$type]['class'] . $appendCss;
-                    $this->options['id'] = $this->getId() . '-' . $type;
-                    echo BootstrapAlert::widget([
-                        'body' => $this->alertTypes[$type]['icon'] . $message,
-                        'closeButton' => $this->closeButton,
-                        'options' => $this->options,
-                    ]);
+                foreach ($data as $i => $message) {
+                    /* initialize css class for each alert box */
+                    $this->options['class'] = $this->alertTypes[$type] . $appendCss;
+                    /* assign unique id to each alert box */
+                    $this->options['id'] = $this->getId() . '-' . $type . '-' . $i;
+                    $appendIcon = isset($this->alertIcons[$type]) ? ' <i class="fa fa-' . $this->alertIcons[$type] . '" aria-hidden="true"></i> ' : '';
+                    echo '
+<div class="alert ' . $this->options['class'] .'" role="alert">
+        <strong>' . $appendIcon . $this->alertMeanings[$type] . '</strong> ' . $message . '
+    </div>';
                 }
-                if ($this->isAjaxRemoveFlash && !\Yii::$app->request->isAjax) {
-                    $session->removeFlash($type);
-                }
+                $session->removeFlash($type);
             }
         }
     }
