98fa6015ea0f454c7c0747755197205c05394b48
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-06-30 12:26:59 +0300

    update use

--- assets/AdminAsset.php
+++ assets/AdminAsset.php
@@ -24,6 +24,6 @@ class AdminAsset extends BaseAdminAsset
 
     ];
     public $depends = [
-        'denchotsanov\assets\AdminLteAssets',
+        'denchotsanov\assets\assets\AdminLteAssets',
     ];
 }
